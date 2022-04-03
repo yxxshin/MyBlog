2eea76899fcfb66732b2397f310d3bcb6be31223
yxxshin <samshin3910@snu.ac.kr>
2022-04-03 16:51:24 +0900

    [FEAT] Modified head.jsx file for noindex

--- themes/icarus/layout/common/head.jsx
+++ themes/icarus/layout/common/head.jsx
@@ -110,6 +110,7 @@ module.exports = class extends Component {
         return <head>
             <meta charset="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+            {page.noindex ? <meta name="robots" content="noindex" /> : null}
             {meta && meta.length ? <MetaTags meta={meta} /> : null}
 
             <title>{getPageTitle(page, config.title, helper)}</title>
